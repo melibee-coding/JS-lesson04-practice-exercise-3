f74d9212951f0c26cb2e89fe840271447c0eea24
melissa_p <melissa.paschuck@gmail.com>
2024-02-15 18:56:04 -0800

    Initial commit.

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Change Instagram icon color
+var instagram = document.querySelector(".fa-instagram");
+instagram.style.color = tomato;
+
+// Add background image
+var foodImage = document.querySelector(".foodie");
+foodImage.style.backgroundImage = "url('../img/victoria-shes-unsplash.jpg')";
+
+// Dotted border-bottom around h1
+var noms = document.querySelector("h1");
+h1.style.borderBottom = "4px dotted tomato";
